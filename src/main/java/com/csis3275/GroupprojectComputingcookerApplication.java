cb32726d6d25d2d0abd6ead1b0493b9af0289a69
Dat Tu <dattuminh@gmail.com>
2021-10-21 19:59:30 -0700

    first commit

---
+++ src/main/java/com/csis3275/GroupprojectComputingcookerApplication.java
@@ -0,0 +1,13 @@
+package com.csis3275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupprojectComputingcookerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupprojectComputingcookerApplication.class, args);
+	}
+
+}
