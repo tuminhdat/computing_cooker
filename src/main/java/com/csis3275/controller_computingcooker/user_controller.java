c8944e2eaaef61e402d0c16209a929bba5f196ad
omar19900 <alkhazraji@student.douglascollege>
2021-10-24 20:49:39 -0700

    implemnting register feature

--- src/main/java/com/csis3275/controller_computingcooker/user_controller.java
+++ src/main/java/com/csis3275/controller_computingcooker/user_controller.java
@@ -1,5 +1,7 @@
 package com.csis3275.controller_computingcooker;
 
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 import javax.servlet.http.HttpSession;
@@ -16,114 +18,66 @@ import org.springframework.web.bind.annotation.RequestParam;
 import com.csis3275.dao_computingcooker.userDAOImpl;
 import com.csis3275.model_computingcooker.user_model;
 
+// for key
+
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 @Controller
 public class user_controller {
 	@Autowired
 	userDAOImpl userDAOImpl;
-	
+
 	public user_model setupAddForm() {
-		
+
 		return new user_model();
 	}
-	
-	@RequestMapping("/users/list")
-	public String showUsers(@ModelAttribute("user")user_model createUser, Model model,HttpSession session) 
-	{
-		
-		ArrayList<user_model> allUsers = userDAOImpl.getAllUsers();
-		
-		ArrayList<String> messages = (ArrayList<String>) session.getAttribute("messages");
-		
-		// Add in the messages, if the api is blank.
-		model.addAttribute("messages", messages != null ? messages : new ArrayList<String>());
-		
-		model.addAttribute("allUsers", allUsers);
-		
-		// Clear the messages before the returning
-		session.removeAttribute("messages");
-		
-		// Return the view
-		return "index";
+
+	@RequestMapping("/")
+	public String showUsers(Model model) {
+		model.addAttribute("user", new user_model());
+
+		return "registration";
 	}
-	
-	
+
 	@PostMapping("/users/create")
-	public String createUsers(@ModelAttribute("user")user_model createUser, Model model,HttpSession session) {
-		
+	public String createUsers(@ModelAttribute("user") user_model createUser, Model model, HttpSession session) {
+
+		// to make key as hashing key
+		createUser.setUserPassword(getMd5(createUser.getUserPassword()));
 		// Add the student
 		userDAOImpl.createUser(createUser);
-		// Populate the message into the sesession
-		ArrayList<String> messages = new ArrayList<String>();
-		messages = session.getAttribute("messages") != null ? (ArrayList<String>) session.getAttribute("messages")
-				: new ArrayList<String>();
-		
-		messages.add("Created User " + createUser.getFirstName());
 
-		session.setAttribute("messages", messages);
-		
-		return "redirect:/users/list";
+		return "index";
 	}
-	
-	
-	// Get the student and display the form
-		@GetMapping("/students/delete/")
-		public String deleteStudent(@RequestParam(required = true) int id, Model model, HttpSession session) {
 
-			// Get the student
-			userDAOImpl.deleteUser(id);
+	// hashing key
+	public static String getMd5(String input) {
+		try {
 
-			// Populate the message into the sesession
-			ArrayList<String> messages = new ArrayList<String>();
-			messages = session.getAttribute("messages") != null ? (ArrayList<String>) session.getAttribute("messages")
-					: new ArrayList<String>();
-			messages.add("Deleted User " + id);
-			session.setAttribute("messages", messages);
+			// Static getInstance method is called with hashing MD5
+			MessageDigest md = MessageDigest.getInstance("MD5");
 
-			return "redirect:/users/list";
+			// digest() method is called to calculate message digest
+			// of an input digest() return array of byte
+			byte[] messageDigest = md.digest(input.getBytes());
+
+			// Convert byte array into signum representation
+			BigInteger no = new BigInteger(1, messageDigest);
+
+			// Convert message digest into hex value
+			String hashtext = no.toString(16);
+			while (hashtext.length() < 32) {
+				hashtext = "0" + hashtext;
+			}
+			return hashtext;
 		}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+		// For specifying wrong message digest algorithms
+		catch (NoSuchAlgorithmException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
--- src/main/java/com/csis3275/dao_computingcooker/userDAOImpl.java
+++ src/main/java/com/csis3275/dao_computingcooker/userDAOImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Service;
 import com.csis3275.model_computingcooker.userRowMapper_computingcooker;
 import com.csis3275.model_computingcooker.user_model;
 
-
 @Service
 public class userDAOImpl {
 
@@ -19,57 +18,18 @@ public class userDAOImpl {
 	JdbcTemplate jdbcTemplate;
 
 	// SQL Queries
-	private final String SQL_GET_ALL_USERS = "SELECT * FROM users";
 	private final String SQL_CREATE_USER = "INSERT INTO users (LastName, FirstName, Age, Email, Description, UserName, UserPassword) VALUES (?, ?, ?, ?, ?, ?, ?)";
-	private final String SQL_DELETE_USER = "DELETE FROM users WHERE id = ?";
-	private final String SQL_UPDATE_USER = "UPDATE users set LastName = ?, FirstName = ?, Age = ?, Email =? , Description = ?, UserName = ?, UserPassword = ? WHERE id = ?";
-	
+
 	// Default Constructor
 	@Autowired
 	public userDAOImpl(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
 	}
-	public ArrayList<user_model> getAllUsers()	{
-		//Empty list for the students
-		ArrayList<user_model> allUsers = new ArrayList<user_model>();
-		
-		//Call the JDBCTemplate, assign ASSIGN the results back.
-		allUsers = (ArrayList<user_model>) jdbcTemplate.query(SQL_GET_ALL_USERS, new userRowMapper_computingcooker());
-		
-		return allUsers;
+
+	// Create a new student by passing one in.
+	public boolean createUser(user_model newUser) {
+		return jdbcTemplate.update(SQL_CREATE_USER, newUser.getLastName(), newUser.getFirstName(), newUser.getAge(),
+				newUser.getEmail(), newUser.getDescription(), newUser.getUserName(), newUser.getUserPassword()) > 0;
 	}
-	
-	//Create a new student by passing one in.
-		public boolean createUser(user_model newUser) {
-			return jdbcTemplate.update(SQL_CREATE_USER, 
-					newUser.getLastName(),
-					newUser.getFirstName(),
-					newUser.getAge(),
-					newUser.getEmail(),
-					newUser.getDescription(),
-					newUser.getUserName(),
-					newUser.getUserPassword()) > 0;
-		}
-		
-		//Delete Student
-		public boolean deleteUser(int id) {
-			return jdbcTemplate.update(SQL_DELETE_USER, id) > 0;
-		}
-		
-		//Update the student (based on the given id)
-		public boolean updateUser(user_model user) {
-			return jdbcTemplate.update(SQL_UPDATE_USER, 
-					user.getLastName(),
-					user.getFirstName(),
-					user.getAge(),
-					user.getEmail(),
-					user.getDescription(),
-					user.getUserName(),
-					user.getUserPassword()) > 0;
-		}
-		
-		
-		
-		
-		
+
 }
