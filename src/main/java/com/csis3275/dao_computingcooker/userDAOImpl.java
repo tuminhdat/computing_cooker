c70c230495bc4849b521179b7e3e6952d6988114
Tri Duc Tran <trant25@student.douglascollege.ca>
2021-10-24 18:25:36 -0700

    Create DAO Implement file

--- src/main/java/com/csis3275/dao_computingcooker/userDAOImpl.java
+++ src/main/java/com/csis3275/dao_computingcooker/userDAOImpl.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
+import com.csis3275.model_computingcooker.userRowMapper_computingcooker;
+import com.csis3275.model_computingcooker.user_model;
+
 @Service
 public class userDAOImpl {
 
@@ -13,11 +16,18 @@ public class userDAOImpl {
 	JdbcTemplate jdbcTemplate;
 
 	// SQL Queries
+	private final String SQL_FIND_USER = "SELECT * FROM users WHERE UserName = ?";
 
 	// Default Constructor
 	@Autowired
 	public userDAOImpl(DataSource dataSource) {
 		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	@SuppressWarnings("deprecation")
+	public user_model findUser(String userName) {
+		return jdbcTemplate.queryForObject(SQL_FIND_USER, new Object[] { userName },
+				new userRowMapper_computingcooker());
 	}
 
 }
