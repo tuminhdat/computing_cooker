d6936bf033e13efc353e4c635ef73601c1d61476
Tri Duc Tran <trant25@student.douglascollege.ca>
2021-10-26 22:51:28 -0700

    Create edit profile view

--- src/main/java/com/csis3275/controller_computingcooker/user_controller.java
+++ src/main/java/com/csis3275/controller_computingcooker/user_controller.java
@@ -1,7 +1,5 @@
 package com.csis3275.controller_computingcooker;
 
-import java.util.ArrayList;
-
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +9,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.csis3275.dao_computingcooker.userDAOImpl;
 import com.csis3275.model_computingcooker.user_model;
 
