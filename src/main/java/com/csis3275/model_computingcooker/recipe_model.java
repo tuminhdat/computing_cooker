0a1cb13ee5c6c68ce3edb8a7bdd527f7f86d4a07
Tri Duc Tran <trant25@student.douglascollege.ca>
2021-11-04 19:59:40 -0700

    Added model and rowmapper for recipe

---
+++ src/main/java/com/csis3275/model_computingcooker/recipe_model.java
@@ -0,0 +1,95 @@
+package com.csis3275.model_computingcooker;
+
+public class recipe_model {
+	private int recipeID;
+	private String recipeTitle;
+	private String description;
+	private int prepTime;
+	private int totalTime;
+	private int numServe;
+	private String ingredient;
+	private String preparation;
+	private String author;
+	private int userID;
+
+	public int getRecipeID() {
+		return recipeID;
+	}
+
+	public void setRecipeID(int recipeID) {
+		this.recipeID = recipeID;
+	}
+
+	public String getRecipeTitle() {
+		return recipeTitle;
+	}
+
+	public void setRecipeTitle(String recipeTitle) {
+		this.recipeTitle = recipeTitle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrepTime() {
+		return prepTime;
+	}
+
+	public void setPrepTime(int prepTime) {
+		this.prepTime = prepTime;
+	}
+
+	public int getTotalTime() {
+		return totalTime;
+	}
+
+	public void setTotalTime(int totalTime) {
+		this.totalTime = totalTime;
+	}
+
+	public int getNumServe() {
+		return numServe;
+	}
+
+	public void setNumServe(int numServe) {
+		this.numServe = numServe;
+	}
+
+	public String getIngredient() {
+		return ingredient;
+	}
+
+	public void setIngredient(String ingredient) {
+		this.ingredient = ingredient;
+	}
+
+	public String getPreparation() {
+		return preparation;
+	}
+
+	public void setPreparation(String preparation) {
+		this.preparation = preparation;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+}
