3a04b2af44f8c3943f0d0ef1322f687c3283184b
Tri Duc Tran <trant25@student.douglascollege.ca>
2021-11-27 15:13:28 -0800

    Create method to insert new comment for recipe and menu

--- src/main/java/com/csis3275/dao_computingcooker/CommentDAOImpl.java
+++ src/main/java/com/csis3275/dao_computingcooker/CommentDAOImpl.java
@@ -5,32 +5,25 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
-import com.csis3275.model_computingcooker.userRowMapper_computingcooker;
-import com.csis3275.model_computingcooker.user_model;
+import com.csis3275.model_computingcooker.Comment_model;
+import com.csis3275.model_computingcooker.CommentRowMapper_computingcooker;
 
 public class CommentDAOImpl {
-	// Use JDPTemplate for implementation
-		JdbcTemplate jdbcTemplate;
-/*
- * CommentID int AUTO_INCREMENT PRIMARY KEY,
-	Content varchar(max),
-	UserID int,
-	RecipeID int,
-	MenuID int
- * 
- * 
- * 
- * */
-		// SQL Queries
-		private final String SQL_CREATE_COMMENT_RECIPE = "INSERT INTO comments (Content, UserID, RecipeID) VALUES (?,?,?)";
-		private final String SQL_CREATE_COMMENT_MENU = "INSERT INTO comments (Content, UserID, MenuID) VALUES (?,?,?)";		
+	JdbcTemplate jdbcTemplate;
 
-		// Default Constructor
-		@Autowired
-		public CommentDAOImpl(DataSource dataSource) {
-			jdbcTemplate = new JdbcTemplate(dataSource);
-		}
-		
-		
+	private final String SQL_CREATE_COMMENT_RECIPE = "INSERT INTO comments (Content, UserID, RecipeID) VALUES (?,?,?)";
+	private final String SQL_CREATE_COMMENT_MENU = "INSERT INTO comments (Content, UserID, MenuID) VALUES (?,?,?)";
 
+	@Autowired
+	public CommentDAOImpl(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	
+	public boolean createCommentForMenu(Comment_model newComment) {
+		return jdbcTemplate.update(SQL_CREATE_COMMENT_MENU, newComment.getContent(), newComment.getUserID(), newComment.getMenuID()) > 0;
+	}
+	
+	public boolean createCommentForRecipe(Comment_model newComment) {
+		return jdbcTemplate.update(SQL_CREATE_COMMENT_MENU, newComment.getContent(), newComment.getUserID(), newComment.getRecipeID()) > 0;
+	}
 }
